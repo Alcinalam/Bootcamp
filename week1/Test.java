eae2b336bc0332124a85a598a75556b292f7821d
Alcinalam <alcinalam@gmail.com>
2024-03-26 10:34:54 +0800

    update

---
+++ week1/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+  public static void main(String[] args) {
+    // Start point of the program
+    // Tool 1: System.out.println()
+    System.out.println("hello! Testing, 中文 $");
+    System.out.println("Testing2");
+
+  }
+}
